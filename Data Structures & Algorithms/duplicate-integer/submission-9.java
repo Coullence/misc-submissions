858c76462b7122ccc0d500508b305ae53c603381
Collins Kibet <38583809+Coullence@users.noreply.github.com>
2026-06-28 18:04:56 +0800

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-9.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        if(nums==null){
+            return false;
+        }
+        int n = nums.length;
+        if(n==1){
+            return false;
+        }
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i=0; i<nums.length;;i++){
+            if(!set.add(nums[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+}
